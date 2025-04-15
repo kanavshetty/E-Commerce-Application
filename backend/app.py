3469d121d0733197c150f3f8d3f751e533079e52
Kanav Shetty <kanavshetty@gmail.com>
2025-04-15 13:16:28 -0400

    changes

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return("Online Store Backend is Running!")
+    return("Online Store Backend is Running!!")
 
 if __name__ == "__main__":
     app.run(debug=True)
